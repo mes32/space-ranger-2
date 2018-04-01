2578db471b7ae124c25bc853bf3548f9c38a8b74
mes32 <stockman.mike@gmail.com>
2018-04-01 10:01:42 -0400

    feature: Add randomization EnemyShipGenerator. Also add a timeout to EnemyShipGenerator so that it doesn't produce an overwhelming supply of enemies.

--- src/main/java/spaceranger/EnemyShipGenerator.java
+++ src/main/java/spaceranger/EnemyShipGenerator.java
@@ -7,11 +7,13 @@
 package spaceranger;
 
 import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
 public class EnemyShipGenerator {
 
-    private static final int[] lane = {
+    private static final int TIME_OUT = 80;
+    private static final int[] RANDOM_LANE = {
         922, 728, 865, 741, 959, 279, 233, 210, 273, 945,
         81, 242, 235, 921, 916, 543, 597, 658, 403, 886,
         830, 686, 643, 287, 877, 905, 718, 852, 968, 18,
@@ -19,31 +21,41 @@ public class EnemyShipGenerator {
         714, 132, 362, 861, 798, 551, 75, 184, 433, 610,
         156, 89, 464, 212, 774, 306, 834, 321, 931, 115,
         151, 963, 751, 716, 608, 134, 773, 150, 714, 332,
-        805, 458, 970, 827, 911, 400, 811, 333, 932, 520,
+        805, 458, 950, 827, 911, 400, 811, 333, 932, 520,
         655, 178, 443, 120, 2, 667, 684, 335, 883, 549,
         528, 839, 886, 774, 91, 527, 797, 598, 832, 359
     };
-    private static final int laneLength = lane.length;
 
     private GameBoard board;
     private int index;
+    private int counter;
 
     EnemyShipGenerator(GameBoard board) {
         this.board = board;
-        this.index = 0;
+        init();
+    }
+
+    private void init() {
+        Random random = new Random();
+        index = random.nextInt(RANDOM_LANE.length);
+        counter = 0;
     }
 
     public void update() {
-        EnemyShip enemyShip = new EnemyShip(board);
-        enemyShip.setInitialPosition(getLane(), -20);
-        board.addSprite(enemyShip);
+        counter++;
+        if (counter >= TIME_OUT) {
+            counter = 0;
+            EnemyShip enemyShip = new EnemyShip(board);
+            enemyShip.setInitialPosition(getLane(), -20);
+            board.addSprite(enemyShip);
+        }
     }
 
     private int getLane() {
         index++;
-        if (index >= laneLength) {
+        if (index >= RANDOM_LANE.length) {
             index = 0;
         }
-        return lane[index];
+        return RANDOM_LANE[index];
     }
 }
