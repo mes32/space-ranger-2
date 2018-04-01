04ea828673907930651c9b381585ee0e7fdab690
mes32 <stockman.mike@gmail.com>
2018-04-01 13:08:58 -0400

    minor: Change the relative location where PlayerMissiles spawn in relation to PlayerShip.

--- src/main/java/spaceranger/PlayerShip.java
+++ src/main/java/spaceranger/PlayerShip.java
@@ -20,8 +20,8 @@ public class PlayerShip extends Sprite {
     public void fireMissile() {
         PlayerMissile missileLeft = new PlayerMissile(board);
         PlayerMissile missileRight = new PlayerMissile(board);
-        missileLeft.setInitialPosition(x, y + 15);
-        missileRight.setInitialPosition(x + width - 5, y + 15);
+        missileLeft.setInitialPosition(x + 10, y + 15);
+        missileRight.setInitialPosition(x + width - 5 - 10, y + 15);
         board.addSprite(missileLeft);
         board.addSprite(missileRight);
     }
