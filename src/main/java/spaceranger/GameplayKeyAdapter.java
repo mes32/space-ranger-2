c886d1eb2c0d9e07b031186f550c171ba7b1483a
mes32 <stockman.mike@gmail.com>
2018-04-14 18:10:23 -0400

    feature: Make the controls respond more smoothly to button mashing.

--- src/main/java/spaceranger/GameplayKeyAdapter.java
+++ src/main/java/spaceranger/GameplayKeyAdapter.java
@@ -15,10 +15,19 @@ public class GameplayKeyAdapter extends KeyAdapter {
 
     private static final int MOVE_PIXELS = 3;
 
+    private boolean movingLeft;
+    private boolean movingRight;
+    private boolean movingDown;
+    private boolean movingUp;
+
     private PlayerShip player;
 
     GameplayKeyAdapter(PlayerShip player) {
         super();
+        movingLeft = false;
+        movingRight = false;
+        movingDown = false;
+        movingUp = false;
         this.player = player;
     }
 
@@ -29,9 +38,13 @@ public class GameplayKeyAdapter extends KeyAdapter {
     @Override
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
-        if (key == KeyEvent.VK_LEFT || key == KeyEvent.VK_RIGHT) {
+        if (key == KeyEvent.VK_LEFT && movingLeft || key == KeyEvent.VK_RIGHT && movingRight) {
+            movingRight = false;
+            movingLeft = false;
             player.setMoveX(0);
-        } else if (key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN) {
+        } else if (key == KeyEvent.VK_UP && movingUp || key == KeyEvent.VK_DOWN && movingDown) {
+            movingUp = false;
+            movingDown = false;
             player.setMoveY(0);
         }
     }
@@ -40,12 +53,20 @@ public class GameplayKeyAdapter extends KeyAdapter {
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         if (key == KeyEvent.VK_LEFT) {
+            movingRight = false;
+            movingLeft = true;
             player.setMoveX(-1 * MOVE_PIXELS);
         } else if (key == KeyEvent.VK_RIGHT) {
+            movingRight = true;
+            movingLeft = false;
             player.setMoveX(MOVE_PIXELS);
         } else if (key == KeyEvent.VK_UP) {
+            movingUp = true;
+            movingDown = false;
             player.setMoveY(-1 * MOVE_PIXELS);
         } else if (key == KeyEvent.VK_DOWN) {
+            movingUp = false;
+            movingDown = true;
             player.setMoveY(MOVE_PIXELS);
         } else if (key == KeyEvent.VK_SPACE) {
             player.fireMissile();
