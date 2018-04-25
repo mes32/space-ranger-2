3b89602b66ef324ef2a8dd97176e94068a06cecb
mes32 <stockman.mike@gmail.com>
2018-04-25 13:33:44 -0400

    feature: Add some visual variety to the sprites used to display EnemySpark.

--- src/main/java/spaceranger/EnemySpark.java
+++ src/main/java/spaceranger/EnemySpark.java
@@ -12,14 +12,25 @@ import javax.swing.*;
 public class EnemySpark extends Sprite {
 
     private static final long DURRATION = 50;
-    private static final String IMAGE_PATH = "/images/EnemySpark.png";
+    private static final String[] IMAGE_PATHS = {
+        "/images/EnemySpark0.png",
+        "/images/EnemySpark1.png",
+        "/images/EnemySpark2.png"
+    };
+    private static final int NUM_IMAGE_PATHS = IMAGE_PATHS.length;
+
+    private static int count = 0;
 
     protected EnemyShip parent;
     protected boolean burntOut;
     protected long startTime;
 
     EnemySpark(GameBoard board, EnemyShip parent) {
-        super(board, IMAGE_PATH);
+        super(board, IMAGE_PATHS[count]);
+        count++;
+        if (count >= NUM_IMAGE_PATHS) {
+            count = 0;
+        }
 
         hitPoints = new HitPoints(1);
         collideDamage = 0;
